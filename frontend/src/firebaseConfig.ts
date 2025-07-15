7187d5d57aed22a8b69bcddbba7af28971eecc26
nenemfvl <nenem.flavia.duck@gmail.com>
2025-07-15 08:15:14 -0300

    chore: trigger redeploy manual

--- frontend/src/firebaseConfig.ts
+++ frontend/src/firebaseConfig.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0tBqi0Et7ngh1vSKXvMPC5ke31uD9yn4",
-  authDomain: "sementesplay-49cf0.firebaseapp.com",
-  projectId: "sementesplay-49cf0",
-  storageBucket: "sementesplay-49cf0.appspot.com",
-  messagingSenderId: "137476416460",
-  appId: "1:137476416460:web:998dfcd668208e478880a1"
+  apiKey: "AIzaSyAm7P5DMB8KY5snKflviigQfm8a97gMsR8",
+  authDomain: "sementesplay-87159.firebaseapp.com",
+  projectId: "sementesplay-87159",
+  storageBucket: "sementesplay-87159.appspot.com",
+  messagingSenderId: "448092780821",
+  appId: "1:448092780821:web:1f6ac44b5279e780ac5c",
+  measurementId: "G-HC9JWL6V4Y"
 };
 
 const app = initializeApp(firebaseConfig);
