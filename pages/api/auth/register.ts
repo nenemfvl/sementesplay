8094aa9e218247dd10c4097fb7779e19dc5201f4
nenemfvl <nenem.flavia.duck@gmail.com>
2025-07-20 12:28:55 -0300

    Remove campo nivelAtual inválido do registro de criador

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -50,7 +50,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           categoria: 'Gaming',
           //seguidores: 0,
           //totalDoacoes: 0,
-          nivelAtual: 'comum',
+          //nivelAtual: 'comum',
           beneficios: '[]'
         }
       })
