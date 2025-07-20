2ad2ae23912c449da17aa7dfb2662bfb3aa36f23
nenemfvl <nenem.flavia.duck@gmail.com>
2025-07-20 13:06:37 -0300

    Corrige uso de motion.div em termos.tsx

--- pages/termos.tsx
+++ pages/termos.tsx
@@ -41,7 +41,7 @@ export default function Termos() {
         </header>
 
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <div
+          <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6 }}
@@ -195,7 +195,7 @@ export default function Termos() {
                 </p>
               </div>
             </motion.div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
