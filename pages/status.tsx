476cd942cc332f8c18f8344ee782f2751040b345
nenemfvl <nenem.flavia.duck@gmail.com>
2025-07-21 20:14:16 -0300

    feat: navbar da home adicionada novamente à página de status

--- pages/status.tsx
+++ pages/status.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Head from 'next/head'
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
 import { auth } from '../lib/auth'
+import Navbar from '../components/Navbar';
 
 export default function Status() {
   const user = typeof window !== 'undefined' ? auth.getUser() : null;
@@ -12,7 +13,7 @@ export default function Status() {
         <meta name="description" content="Status do sistema SementesPLAY" />
       </Head>
       <div className="min-h-screen bg-sss-dark">
-        {/* Navbar removida */}
+        <Navbar />
         {/* Conteúdo da página de status */}
         <div className="flex flex-col items-center justify-center py-16">
           <div className="bg-sss-medium rounded-lg shadow-lg p-8 border border-sss-light text-center">
