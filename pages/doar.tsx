03ea9feb8ddf9462f55c1caec0eba6272d8467df
nenemfvl <nenem.flavia.duck@gmail.com>
2025-07-25 20:17:14 -0300

    feat: remove botão voltar ao dashboard da página de doação

--- pages/doar.tsx
+++ pages/doar.tsx
@@ -114,13 +114,6 @@ export default function Doar() {
         <Navbar />
 
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          {/* Botão Voltar */}
-          <div className="mb-6">
-            <Link href="/dashboard" className="inline-flex items-center text-sss-accent hover:text-red-400 transition-colors">
-              <ArrowLeftIcon className="w-5 h-5 mr-2" />
-              Voltar ao Dashboard
-            </Link>
-          </div>
           
           <div className="space-y-8">
             {/* Header */}
