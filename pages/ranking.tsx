6d4cd79cf8678f56b2cc8ef4725a95d7668d62c1
nenemfvl <nenem.flavia.duck@gmail.com>
2025-07-25 18:37:04 -0300

    fix: altera texto do botão de 'Voltar ao Dashboard' para 'Voltar ao Status' na página de ranking

--- pages/ranking.tsx
+++ pages/ranking.tsx
@@ -239,7 +239,7 @@ export default function Ranking() {
               <div className="flex items-center space-x-4">
                 <Link href="/status" className="inline-flex items-center text-sss-accent hover:text-red-400">
                   <ArrowLeftIcon className="w-5 h-5 mr-2" />
-                  Voltar ao Dashboard
+                  Voltar ao Status
                 </Link>
               </div>
               
