cf19ce31bf87d66d2727744654cad8f376616f66
nenemfvl <nenem.flavia.duck@gmail.com>
2025-07-31 11:28:29 -0300

    Remover sistema PIX antigo - manter apenas Mercado Pago

--- pages/api/test-pix.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ 
-    message: 'API raiz funcionando!',
-    method: req.method,
-    timestamp: new Date().toISOString()
-  })
-} 
