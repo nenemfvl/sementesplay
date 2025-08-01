19202c69526d429c9c1dd8914d0a84821872614b
nenemfvl <nenem.flavia.duck@gmail.com>
2025-07-31 21:35:23 -0300

    Corrige erros de linting: meta tags e imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,11 +36,9 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#dc2626" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#dc2626" media="(prefers-color-scheme: light)" />
-        <meta name="theme-color" content="#dc2626" media="(prefers-color-scheme: dark)" />
+        <meta name="theme-color" content="#dc2626" />
 
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
